63991d0e57b074a8a7a420260ec896bde7ff8701
doratslin <u10150072@lssh.tp.edu.tw>
2017-02-23 17:10:01 +0800

    first commit

---
+++ src/main.java
@@ -0,0 +1,10 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by dora-lin on 2017/2/23.
+ */
+public class main {
+    public static void main(String[] args){
+        System.out.println("104080002");
+    }
+}
